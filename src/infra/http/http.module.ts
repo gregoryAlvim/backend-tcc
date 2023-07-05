7da129b5476db3049371dd4efae46c86b75b66e2
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-05 19:30:41 -0300

    Feat: imported UserAuthModule to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user.module';
 import { PrismaClient } from '@prisma/client';
+import { UserAuthModule } from './modules/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserAuthModule, UserModule],
   controllers: [],
   providers: [PrismaClient],
 })
