aad0c53bff8585aa96c3afdf84665237af03a067
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-05 19:19:20 -0300

    Feat: added methods on user repository, findById and checkIfUserExistsByEmail

--- src/application/repositories/user-repository.ts
+++ src/application/repositories/user-repository.ts
@@ -3,8 +3,8 @@ import { User } from '../entities/user.entity';
 
 export abstract class UserRepository {
   abstract create(user: User, wallet: Wallet): Promise<void>;
-  // abstract findAll(): Promise<User[] | null>;
-  // abstract findById(userId: string): Promise<User | null>;
+  abstract findById(userId: string): Promise<User | null>;
+  abstract checkIfUserExistsByEmail(email: string): Promise<User | null>;
   // abstract update(user: User): Promise<void>;
   // abstract remove(userId: string): Promise<void>;
 }
