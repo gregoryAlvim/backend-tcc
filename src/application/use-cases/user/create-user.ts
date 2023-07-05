65fa3bd53aa7898512a01994acae8c3c20723e9a
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-05 19:28:26 -0300

    Feat: created verification to check if user already exists

--- src/application/use-cases/user/create-user.ts
+++ src/application/use-cases/user/create-user.ts
@@ -1,8 +1,9 @@
 import { Wallet } from '@application/entities/wallet.entity';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { User } from 'src/application/entities/user.entity';
 import { UserRepository } from 'src/application/repositories/user-repository';
 import * as bcrypt from 'bcrypt';
+import { CheckIfUserExistsByEmail } from './check-if-user-exists-by-email';
 
 interface CreateUserRequest {
   name: string;
@@ -12,21 +13,31 @@ interface CreateUserRequest {
 }
 
 interface CreateUserResponse {
-  user: User;
+  user: User | null;
 }
 
 @Injectable()
 export class CreateUser {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private checkIfUserExistsByEmail: CheckIfUserExistsByEmail,
+  ) {}
 
   async execute(request: CreateUserRequest): Promise<CreateUserResponse> {
     const { name, email, password, avatar } = request;
+
+    const { userExists } = await this.checkIfUserExistsByEmail.execute({
+      email,
+    });
+
+    if (userExists) {
+      throw new HttpException('Este email já está sendo utilizado!', 409);
+    }
 
     const saltOrRounds = 8;
     const cryptPassword = await bcrypt.hash(password, saltOrRounds);
 
     const wallet = new Wallet({ value: 0 });
-
     const user = new User({ name, email, password: cryptPassword, avatar });
 
     await this.userRepository.create(user, wallet);
