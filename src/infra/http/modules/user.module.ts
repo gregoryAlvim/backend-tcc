3f5cfeb8a78f3441bf97b00aa6984011ccb9acc1
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-07 16:16:48 -0300

    Feat: added UpdateUserById use case

--- src/infra/http/modules/user.module.ts
+++ src/infra/http/modules/user.module.ts
@@ -4,10 +4,16 @@ import { CreateUser } from 'src/application/use-cases/user/create-user';
 import { DatabaseModule } from 'src/infra/database/prisma/database.module';
 import { FindUserById } from '@application/use-cases/user/find-user-by-id';
 import { CheckIfUserExistsByEmail } from '@application/use-cases/user/check-if-user-exists-by-email';
+import { UpdateUserById } from '@application/use-cases/user/update-user-by-id';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [CreateUser, FindUserById, CheckIfUserExistsByEmail],
+  providers: [
+    CreateUser,
+    FindUserById,
+    CheckIfUserExistsByEmail,
+    UpdateUserById,
+  ],
 })
 export class UserModule {}
