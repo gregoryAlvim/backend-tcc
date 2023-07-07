6fc67b3d53c4e45b06f2aa3a83a0d7bb3a878b45
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-07 16:21:27 -0300

    Feat: created category entity

--- src/application/entities/category.entity.ts
+++ src/application/entities/category.entity.ts
@@ -1,6 +1,48 @@
+import { Replace } from '@helpers/Replace';
+import { randomUUID } from 'crypto';
+
 export interface CategoryProps {
-  id: string;
   createdAt: Date;
   name: string;
   type: string;
+}
+
+export class Category {
+  private _id: string;
+  private props: CategoryProps;
+
+  constructor(
+    props: Replace<CategoryProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set name(name: string) {
+    this.props.name = name;
+  }
+
+  public get name(): string {
+    return this.props.name;
+  }
+
+  public set type(type: string) {
+    this.props.type = type;
+  }
+
+  public get type(): string {
+    return this.props.type;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
 }
