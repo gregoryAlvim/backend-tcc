47174bc7a57b4d1c433f2b1b3a5649c436162843
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-07 16:11:58 -0300

    Feat: added new logic on method create to create the default categories, and added e implemented the update method on repository

--- src/infra/database/prisma/repositories/prisma-user-repository.ts
+++ src/infra/database/prisma/repositories/prisma-user-repository.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@application/entities/user.entity';
 import { UserRepository } from 'src/application/repositories/user-repository';
-import { PrismaService } from '../../prisma.service';
+
 import { PrismaUserMapper } from '../mappers/prisma-user-mapper';
 import { Wallet } from '@application/entities/wallet.entity';
 import { PrismaWalletMapper } from '../mappers/prisma-wallet-mapper';
+import { Category } from '@application/entities/category.entity';
+import { PrismaCategoryMapper } from '../mappers/prisma-category-mapper';
+import { PrismaService } from '@infra/database/prisma.service';
 
 @Injectable()
 export class PrismaUserRepository implements UserRepository {
   constructor(private prisma: PrismaService) {}
 
-  async create(user: User, wallet: Wallet): Promise<void> {
+  async create(
+    user: User,
+    wallet: Wallet,
+    categories: Category[],
+  ): Promise<void> {
     const userRaw = PrismaUserMapper.toPrisma(user);
     const walletRaw = PrismaWalletMapper.toPrisma(wallet);
+
+    const categoriesRaw = categories.map((category) =>
+      PrismaCategoryMapper.toPrisma(category),
+    );
 
     await this.prisma.user.create({
       data: {
@@ -22,6 +33,11 @@ export class PrismaUserRepository implements UserRepository {
             ...walletRaw,
           },
         },
+        Category: {
+          createMany: {
+            data: categoriesRaw,
+          },
+        },
       },
       include: {
         Wallet: true,
@@ -29,10 +45,10 @@ export class PrismaUserRepository implements UserRepository {
     });
   }
 
-  async findById(userId: string): Promise<User | null> {
+  async findById(user_uuid: string): Promise<User | null> {
     const user = await this.prisma.user.findUnique({
       where: {
-        id: userId,
+        id: user_uuid,
       },
     });
 
@@ -55,6 +71,17 @@ export class PrismaUserRepository implements UserRepository {
     }
 
     return PrismaUserMapper.toDomain(user);
+  }
+
+  async update(user: User): Promise<void> {
+    const userRaw = PrismaUserMapper.toPrisma(user);
+
+    await this.prisma.user.update({
+      where: {
+        id: userRaw.id,
+      },
+      data: userRaw,
+    });
   }
 
   // async update(user: User): Promise<void> {}
