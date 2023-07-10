2b6ea5c6a2514a82279f2bb33e2af802cd618b1e
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-10 18:37:43 -0300

    Feat: created repository for the categories

--- src/application/repositories/category-repository.ts
+++ src/application/repositories/category-repository.ts
@@ -1,7 +1,8 @@
 import { Category } from '@application/entities/category.entity';
 
 export abstract class CategoryRepository {
-  abstract create(category: Category): Promise<void>;
+  abstract create(user_uuid: string, category: Category): Promise<void>;
   abstract update(category: Category): Promise<void>;
-  abstract getAllCategoriesByType(type: string): Promise<Category[]>;
+  abstract findCategoryById(category_uuid: string): Promise<Category | null>;
+  abstract getAllCategories(user_uuid: string): Promise<Category[]>;
 }
