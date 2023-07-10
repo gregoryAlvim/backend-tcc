79088c9bdbe2d072d3311470ee03adee383a29b3
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-10 18:36:11 -0300

    Refactor: little changes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,10 @@ import { UserModule } from './modules/user.module';
 import { PrismaClient } from '@prisma/client';
 import { UserAuthModule } from './modules/auth.module';
 import { WalletModule } from './modules/wallet.module';
+import { CategoryModule } from './modules/category.module';
 
 @Module({
-  imports: [UserAuthModule, UserModule, WalletModule],
-  controllers: [],
+  imports: [UserAuthModule, UserModule, WalletModule, CategoryModule],
   providers: [PrismaClient],
 })
 export class httpModule {}
