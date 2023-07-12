8c8d48895d25434d9c1fb0e3a5ed962eb4bf1e08
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-12 16:27:59 -0300

    Feat: Implemented the constructor, getters and setters

--- src/application/entities/income.entity.ts
+++ src/application/entities/income.entity.ts
@@ -1,11 +1,73 @@
-import { CategoryProps } from './category.entity';
+import { Replace } from '@helpers/Replace';
+import { randomUUID } from 'crypto';
+import { Category } from './category.entity';
 
 export interface IncomeProps {
-  id: string;
-  createdAt: Date;
-  value: number;
-  isReceived: boolean;
   date: Date;
-  Description: string;
-  category: CategoryProps;
+  value: number;
+  createdAt: Date;
+  category: Category;
+  isReceived: boolean;
+  description: string;
+}
+
+export class Income {
+  private _id: string;
+  private props: IncomeProps;
+
+  constructor(props: Replace<IncomeProps, { createdAt?: Date }>, id?: string) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set value(value: number) {
+    this.props.value = value;
+  }
+
+  public get value(): number {
+    return this.props.value;
+  }
+
+  public set isReceived(isReceived: boolean) {
+    this.props.isReceived = isReceived;
+  }
+
+  public get isReceived(): boolean {
+    return this.props.isReceived;
+  }
+
+  public set date(date: Date) {
+    this.props.date = date;
+  }
+
+  public get date(): Date {
+    return this.props.date;
+  }
+
+  public set description(description: string) {
+    this.props.description = description;
+  }
+
+  public get description(): string {
+    return this.props.description;
+  }
+
+  public set category(category: Category) {
+    this.props.category = category;
+  }
+
+  public get category(): Category {
+    return this.props.category;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
 }
