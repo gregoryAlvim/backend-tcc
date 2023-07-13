0cb50fe41040f817bfda7d672d503bc658675129
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-13 18:39:27 -0300

    Feat: added GetIncomeOfMonth on providers

--- src/infra/http/modules/income.module.ts
+++ src/infra/http/modules/income.module.ts
@@ -4,10 +4,16 @@ import { DatabaseModule } from '@infra/database/prisma/database.module';
 import { CreateIncome } from '@application/use-cases/income/create-income';
 import { FindIncomeById } from '@application/use-cases/income/find-income-by-id';
 import { UpdateIncomeById } from '@application/use-cases/income/update-income-by-id';
+import { GetIncomesOfMonth } from '@application/use-cases/income/get-incomes-of-month';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [IncomeController],
-  providers: [CreateIncome, FindIncomeById, UpdateIncomeById],
+  providers: [
+    CreateIncome,
+    FindIncomeById,
+    UpdateIncomeById,
+    GetIncomesOfMonth,
+  ],
 })
 export class IncomeModule {}
