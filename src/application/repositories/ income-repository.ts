89bffde0afd41c6fb6d6c6259d24850c764c076f
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-13 18:33:49 -0300

    Feat: added abstract method getIncomesOfMonth

--- src/application/repositories/ income-repository.ts
+++ src/application/repositories/ income-repository.ts
@@ -4,4 +4,9 @@ export abstract class IncomeRepository {
   abstract create(user_uuid: string, income: Income): Promise<void>;
   abstract update(income: Income): Promise<void>;
   abstract findIncomeById(income_uuid: string): Promise<Income | null>;
+  abstract getIncomesOfMonth(
+    user_uuid: string,
+    initialDate: Date,
+    finalDate: Date,
+  ): Promise<Income[]>;
 }
