282a8ad592d1f90c763caac008f4922e7baa84ce
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-14 14:11:46 -0300

    Feat: created expense entity

--- src/application/entities/expense.entity.ts
+++ src/application/entities/expense.entity.ts
@@ -1,11 +1,73 @@
-import { CategoryProps } from './category.entity';
+import { randomUUID } from 'crypto';
+import { Replace } from '@helpers/Replace';
+import { Category } from './category.entity';
 
 export interface ExpenseProps {
-  id: string;
-  createdAt: Date;
+  date: Date;
   value: number;
   isPay: boolean;
-  date: Date;
-  Description: string;
-  category: CategoryProps;
+  createdAt: Date;
+  category: Category;
+  description: string;
+}
+
+export class Expense {
+  private _id: string;
+  private props: ExpenseProps;
+
+  constructor(props: Replace<ExpenseProps, { createdAt?: Date }>, id?: string) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set value(value: number) {
+    this.props.value = value;
+  }
+
+  public get value(): number {
+    return this.props.value;
+  }
+
+  public set isPay(isPay: boolean) {
+    this.props.isPay = isPay;
+  }
+
+  public get isPay(): boolean {
+    return this.props.isPay;
+  }
+
+  public set date(date: Date) {
+    this.props.date = date;
+  }
+
+  public get date(): Date {
+    return this.props.date;
+  }
+
+  public set description(description: string) {
+    this.props.description = description;
+  }
+
+  public get description(): string {
+    return this.props.description;
+  }
+
+  public set category(category: Category) {
+    this.props.category = category;
+  }
+
+  public get category(): Category {
+    return this.props.category;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
 }
