e583962ddb5170c514f7cbbde18b4b7913844e2f
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-19 15:43:50 -0300

    Feat: created planning module

--- src/infra/http/modules/planning.module.ts
+++ src/infra/http/modules/planning.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/prisma/database.module';
 import { PlanningController } from '../controllers/planning.controller';
 import { CreatePlanning } from '@application/use-cases/planning/create-planning';
+import { FindPlanningById } from '@application/use-cases/planning/find-a-planning-by-id';
+import { DeletePlanningById } from '@application/use-cases/planning/delete-a-planning-by-id';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [PlanningController],
-  providers: [CreatePlanning],
+  providers: [CreatePlanning, FindPlanningById, DeletePlanningById],
 })
 export class PlanningModule {}
