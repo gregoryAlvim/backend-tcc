14171bd63bcba7eccfce3d2b5db5e059cbf5a24f
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-31 10:29:04 -0300

    Feat: created create objective body

---
+++ src/infra/http/dtos/objective/create-objective-body.ts
@@ -0,0 +1,27 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateObjectiveBody {
+  @IsNotEmpty({
+    message: 'The date should not be empty.',
+  })
+  date: string;
+
+  @IsNotEmpty({
+    message: 'The goal should not be empty.',
+  })
+  goal: number;
+
+  @IsNotEmpty({
+    message: 'The initialValue should not be empty.',
+  })
+  initialValue: number;
+
+  @IsNotEmpty({
+    message: 'The suggestion should not be empty.',
+  })
+  suggestion: {
+    name: string;
+    amountParcels: number;
+    valueOfParcels: number;
+  };
+}
