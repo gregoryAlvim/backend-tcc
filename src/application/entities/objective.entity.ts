f6cf2fb36fb9d665fbe0c06c0f66ba5318598225
Grégory Alvim <gregori.jagua@gmail.com>
2023-07-31 10:24:45 -0300

    Feat: created getter and setter to the objective entity

--- src/application/entities/objective.entity.ts
+++ src/application/entities/objective.entity.ts
@@ -1,12 +1,68 @@
-import { ParcelProps } from './parcel.entity';
+import { randomUUID } from 'crypto';
+import { Parcel } from './parcel.entity';
+import { Replace } from '@helpers/Replace';
 
 export interface ObjectiveProps {
-  id: string;
   createdAt: Date;
   date: Date;
   goal: number;
   initialValue: number;
-  parcels: ParcelProps[];
+  parcels: Parcel[];
 }
 
-export class Objective {}
+export class Objective {
+  private _id: string;
+  private props: ObjectiveProps;
+
+  constructor(
+    props: Replace<ObjectiveProps, { createdAt?: Date; parcels?: Parcel[] }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      parcels: props.parcels ?? [],
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set date(date: Date) {
+    this.props.date = date;
+  }
+
+  public get date(): Date {
+    return this.props.date;
+  }
+
+  public set goal(goal: number) {
+    this.props.goal = goal;
+  }
+
+  public get goal(): number {
+    return this.props.goal;
+  }
+
+  public set initialValue(initialValue: number) {
+    this.props.initialValue = initialValue;
+  }
+
+  public get initialValue(): number {
+    return this.props.initialValue;
+  }
+
+  public set parcels(parcel: Parcel) {
+    this.props.parcels.push(parcel);
+  }
+
+  public get parcels(): Parcel[] {
+    return this.props.parcels;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
