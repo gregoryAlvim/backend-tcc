1543f50e44e345b9167db5aa0abe95c6d3c89d6f
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-01 09:23:25 -0300

    Feat: added all providers to objective controller

--- src/infra/http/modules/objective.module.ts
+++ src/infra/http/modules/objective.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/prisma/database.module';
 import { ObjectiveController } from '../controllers/objective.controller';
-import { GetSmartBookingSuggestion } from '@application/use-cases/objective/get-smart-booking-suggestion';
 import { CreateObjective } from '@application/use-cases/objective/create-objective';
+import { GetAllObjectives } from '@application/use-cases/objective/get-all-objectives';
+import { FindObjectiveById } from '@application/use-cases/objective/find-an-objective-by-id';
+import { DeleteObjectiveById } from '@application/use-cases/objective/delete-an-objective-by-id';
+import { GetSmartBookingSuggestion } from '@application/use-cases/objective/get-smart-booking-suggestion';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ObjectiveController],
-  providers: [GetSmartBookingSuggestion, CreateObjective],
+  providers: [
+    CreateObjective,
+    GetAllObjectives,
+    FindObjectiveById,
+    DeleteObjectiveById,
+    GetSmartBookingSuggestion,
+  ],
 })
 export class ObjectiveModule {}
