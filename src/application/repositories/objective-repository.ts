9767da67be83b8c703c4c5422acadffc8b26e5c6
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-01 09:21:18 -0300

    Feat: added the get all objectives abstract method

--- src/application/repositories/objective-repository.ts
+++ src/application/repositories/objective-repository.ts
@@ -3,5 +3,6 @@ import { Objective } from '@application/entities/objective.entity';
 export abstract class ObjectiveRepository {
   abstract create(user_uuid: string, objective: Objective): Promise<void>;
   abstract findObjectiveById(objective_uuid: string): Promise<Objective | null>;
+  abstract getAllObjectives(user_uuid: string): Promise<Objective[] | null>;
   abstract delete(objective_uuid: string): Promise<void>;
 }
