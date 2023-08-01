d2afe9582a0f1da3186c6cd1aa64f9005a12a7fa
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-01 09:23:46 -0300

    Feat: created objective view model

---
+++ src/infra/http/view-models/objective-view-model.ts
@@ -0,0 +1,18 @@
+import { ParcelViewModel } from './parcel-view-model';
+import { Objective } from '@application/entities/objective.entity';
+
+export class ObjectiveViewModel {
+  static toHTTP(objective: Objective) {
+    return {
+      id: objective.id,
+      goal: objective.goal,
+      date: objective.date,
+      createdAt: objective.createdAt,
+      description: objective.description,
+      initialValue: objective.initialValue,
+      parcels: objective.parcels.map((parcel) =>
+        ParcelViewModel.toHTTP(parcel),
+      ),
+    };
+  }
+}
