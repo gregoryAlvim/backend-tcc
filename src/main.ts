2695a41c54a33a7e9fbe23c07b0b29fff7e5de1b
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-08 15:48:36 -0300

    Build: atived cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    cors: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
