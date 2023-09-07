a55af07dfc958cb9211c298840fe8a2c57d7c7e3
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-07 12:57:24 -0300

    Feat: added get all plannings method

--- src/application/repositories/planning-repository.ts
+++ src/application/repositories/planning-repository.ts
@@ -3,5 +3,6 @@ import { Planning } from '@application/entities/planning.entity';
 export abstract class PlanningRepository {
   abstract create(user_uuid: string, planning: Planning): Promise<void>;
   abstract findPlanningById(planning_uuid: string): Promise<Planning | null>;
+  abstract getAllPlannings(user_uuid: string): Promise<Planning[]>;
   abstract delete(planning_uuid: string): Promise<void>;
 }
